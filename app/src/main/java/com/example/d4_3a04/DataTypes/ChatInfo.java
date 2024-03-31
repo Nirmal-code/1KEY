5084fde0d18232ddc7b691bc9c4ef7bc42c3606d
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2024-03-31 12:22:33 -0400

    [Update] Added Chat Info Datatypes
    Datatype declarations

---
+++ app/src/main/java/com/example/d4_3a04/DataTypes/ChatInfo.java
@@ -0,0 +1,43 @@
+package com.example.d4_3a04.DataTypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatInfo implements InfoEntity{
+    private List<String> employee_ids;
+    private List<LogEntity> log_history;
+    private SingleChatProvider provider;
+
+
+    public ChatInfo(SingleChatProvider new_provider){
+        this.provider = new_provider;
+        this.employee_ids = new ArrayList<>();
+        this.log_history = new ArrayList<LogEntity>();
+    }
+
+    @Override
+    public InfoType getInfoType() {
+        return InfoType.Log;
+    }
+
+    public List<String> getEmployee_ids(){
+        return this.employee_ids;
+    }
+
+    public SingleChatProvider getProvider(){
+        return this.provider;
+    }
+
+    public List<LogEntity> getLog_history(){
+        return this.log_history;
+    }
+
+    public void addLog(LogEntity entity){
+        log_history.add(entity);
+    }
+
+    public void addUser(String UUID){
+        employee_ids.add(UUID);
+    }
+
+}
---
+++ app/src/main/java/com/example/d4_3a04/DataTypes/LogEntity.java
@@ -0,0 +1,24 @@
+package com.example.d4_3a04.DataTypes;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class LogEntity {
+    public String employee_id;
+    public String message;
+    public Date date;
+    public Time time;
+    public String key;
+
+    public LogEntity(String employee_id, String message, Date date, Time time, String key){
+        this.employee_id = employee_id;
+        this.message = message;
+        this.date = date;
+        this.time = time;
+        this.key = key;
+    }
+
+    public String getMessage(){
+        return this.message;
+    }
+}
