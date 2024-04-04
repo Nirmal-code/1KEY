12b05d1b4604c085262b446f97ca0d1e739a6791
Nirmal-code <nirmal.chaudhari2003@gmail.com>
2024-04-03 22:33:15 -0400

    [Feature] User can logout
    Updated code to allow logout in app menu.

--- app/src/main/java/com/example/d4_3a04/BrowseActiveChats.java
+++ app/src/main/java/com/example/d4_3a04/BrowseActiveChats.java
@@ -3,12 +3,16 @@ package com.example.d4_3a04;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.d4_3a04.AccountManager.LoginPage;
 import com.example.d4_3a04.databinding.SearchForBinding;
 import com.example.d4_3a04.DataTypes.ChatInfo;
 import com.example.d4_3a04.SingleChatProvider.SingleChatManager;
@@ -98,5 +102,27 @@ public class BrowseActiveChats extends AppCompatActivity {
             }
         });
 
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void inflateLogin(){
+        Intent intent = new Intent(BrowseActiveChats.this, LoginPage.class);
+        this.startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection.
+
+        if (item.getItemId()==R.id.log_out){
+            inflateLogin();
+            return true;
+        }else{
+            return false;
+        }
     }
 }
